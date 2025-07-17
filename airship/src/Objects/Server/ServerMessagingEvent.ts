3d2458a50a746a82f3a574b15f7bfdba7724bbcc
Jonathan Holmes <Vorlias@users.noreply.github.com>
2025-07-17 13:23:58 +1200

    Support Messaging events for Airship

--- airship/src/Objects/Server/ServerMessagingEvent.ts
+++ airship/src/Objects/Server/ServerMessagingEvent.ts
@@ -1,17 +1,70 @@
 import { Connection } from "@Vorlias/NexusNet/Core/Types/Dist";
 import { CrossServerEventDeclaration } from "@Vorlias/NexusNet/Core/Types/NetworkObjectModel";
 import { ServerBroadcaster } from "@Vorlias/NexusNet/Core/Types/Server/NetworkObjects";
+import { AirshipScriptConnection } from "../NetConnection";
+import { Platform } from "@Easy/Core/Shared/Airship";
+import { Game } from "@Easy/Core/Shared/Game";
+import { Signal } from "@Easy/Core/Shared/Util/Signal";
+
+interface ServerBroadcastMessage {
+	readonly sourceServerId: string;
+	readonly targetServerId: string | undefined;
+	readonly args: unknown[];
+}
+
+export function isServerBroadcastMessage(value: unknown): value is ServerBroadcastMessage {
+	return typeIs(value, "table") && "sourceServerId" in value && "args" in value;
+}
 
 export class ServerMessagingEvent<TArgs extends unknown[]> implements ServerBroadcaster<TArgs> {
-	public constructor(private readonly name: string, declaration: CrossServerEventDeclaration<TArgs>) {
-		throw `Not yet implemented on the Airship side`; // jonah is working on this anyhow.
+	private readonly topic: string;
+	private readonly onMockBroadcast = new Signal<[data: ServerBroadcastMessage]>();
+
+	public constructor(name: string, declaration: CrossServerEventDeclaration<TArgs>) {
+		this.topic = `nexus-${name.gsub("/", "-")[0]}`;
 	}
 
-	BroadcastAllServers(...message: TArgs): void {}
+	private GetBroadcastData(targetServerId: string | undefined, args: TArgs): ServerBroadcastMessage {
+		let data: ServerBroadcastMessage = {
+			sourceServerId: Game.serverId,
+			targetServerId,
+			args,
+		};
+		return data;
+	}
 
-	BroadcastToServer(serverId: string, ...message: TArgs): void {}
+	public BroadcastAllServers(...message: TArgs): void {
+		if (Game.IsEditor()) {
+			this.onMockBroadcast.Fire(this.GetBroadcastData(undefined, message));
+		} else {
+			Platform.Server.Messaging.Publish(this.topic, this.GetBroadcastData(undefined, message));
+		}
+	}
 
-	Connect(callback: (serverId: string, ...message: TArgs) => void): Connection {
-		throw `TODO: Implement`;
+	public BroadcastToServer(serverId: string, ...message: TArgs): void {
+		if (Game.IsEditor()) {
+			this.onMockBroadcast.Fire(this.GetBroadcastData(serverId, message));
+		} else {
+			Platform.Server.Messaging.Publish(this.topic, this.GetBroadcastData(serverId, message));
+		}
+	}
+
+	public Connect(callback: (serverId: string, ...message: TArgs) => void): Connection {
+		if (Game.IsEditor()) {
+			const unsubscribe = this.onMockBroadcast.Connect((data) => {});
+			return new AirshipScriptConnection(unsubscribe);
+		} else {
+			const subscription = Platform.Server.Messaging.Subscribe(this.topic, (data) => {
+				if (!isServerBroadcastMessage(data)) return;
+
+				if (data.targetServerId === undefined || data.targetServerId === Game.serverId) {
+					callback(data.sourceServerId, ...(data.args as TArgs));
+				}
+			});
+
+			return new AirshipScriptConnection(() => {
+				subscription.unsubscribe();
+			});
+		}
 	}
 }
